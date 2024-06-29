e4137a734218b06830c279727385e25a228ff5e6
matiastartara <19376841+matiastartara@users.noreply.github.com>
2024-06-29 17:18:10 -0300

    add invalid login test (#1)
    Co-authored-by: Matias <Matias@LAPTOP-SOREGMCP>

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,20 +7,31 @@ describe('Login test', () => {
     cy.visit('/')
     cy.intercept('GET', '/web/index.php/api/v2/dashboard/employees/locations').as('getLocations');
 
-    cy.fixture('loginCredentials').then((login) => {
-      cy.login("Admin", "admin123");
+    cy.fixture('valid_login_credentials').then((login) => {
+      cy.login(login.email, login.password);
     })
 
-     // Wait for the intercepted request to occur
-     cy.wait('@getLocations').then((interception) => {
-      // Access the intercepted request details
-      const request = interception.request;
+    // Wait for the intercepted request to occur
+    cy.wait('@getLocations').then((interception) => {
       const response = interception.response;
-
       expect(response.statusCode).to.equal(200);
     });
 
-    cy.url().should('include','/dashboard/index');
+    cy.url().should('include', '/dashboard/index');
 
   })
+
+
+  it('Invalid login user and password test', () => {
+   
+    cy.visit('/')
+    cy.fixture('invalid_login_credentials').then((login) => {
+      cy.login(login.email, login.password);
+    })
+
+    cy.get('.oxd-alert-content > .oxd-text').contains('Invalid credentials');
+    cy.url().should('include', '/web/index.php/auth/login');
+
+  });
+
 })
